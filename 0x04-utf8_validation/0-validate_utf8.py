99fb8a59edcf83edeb61cccd246aba70da152b79
aloicerains <aloiceokoth98@gmail.com>
2022-05-04 15:54:00 +0300

    differnt approach

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""UTF-8 Validation"""
+"""UTF-8 Validator"""
 
 
 def validUTF8(data):
-    """Function validates the data"""
-    no_bytes = 0
-    for num in data:
-        binary = format(num, '#010b')[-8:]
-        if no_bytes == 0:
-            for bit in binary:
-                if bit == '0':
-                    break
-                no_bytes += 1
-            if no_bytes == 0:
-                continue  # one byte character
-            if no_bytes == 1 or no_bytes > 4:
+    """Function validates utf-8"""
+    counter = 0
+    for elem in data:
+        if counter == 0:
+            if elem & 128 == 0:
+                counter = 0
+            elif elem & 224 == 192:
+                counter = 1
+            elif elem & 240 == 224:
+                counter = 2
+            elif elem & 248 == 240:
+                counter = 3
+            else:
                 return False
         else:
-            # process integes to adhere to pattern '10xxxx'
-            if not (binary[0] == '1' and binary[1] == '0'):
+            if elem & 192 != 128:
                 return False
-            no_bytes -= 1
-            return no_bytes == 0
-    return True
+            counter -= 1
+    if counter == 0:
+        return True
+    return False
